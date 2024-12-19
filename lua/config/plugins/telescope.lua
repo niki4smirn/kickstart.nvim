e3635ca7f270bd7ed2c56ad74ca3b3c3932808c0
Nikita Smirnov <kitos.smirnov@gmail.com>
2024-12-19 15:57:12 +0100

    adjusting telescope

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -15,6 +15,21 @@ return {
         local telescope = require 'telescope'
 
         telescope.setup {
+          defaults = {
+            layout_config = {
+              horizontal = {
+                preview_width = 0.6,
+              },
+            },
+          },
+          pickers = {
+            find_files = {
+              theme = 'ivy',
+              layout_config = {
+                height = 0.7,
+              },
+            },
+          },
           extensions = {
             fzf = {},
             ['ui-select'] = {
@@ -30,6 +45,20 @@ return {
         vim.keymap.set('n', '<leader>sg', builtin.live_grep)
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics)
         vim.keymap.set('n', '<leader>sr', builtin.resume)
+        vim.keymap.set('n', '<leader>sh', builtin.help_tags)
+
+        vim.keymap.set('n', '<leader>gs', function()
+          local current_buffer_path = vim.api.nvim_buf_get_name(0)
+          local relative_path = vim.fn.fnamemodify(current_buffer_path, ':.')
+
+          -- Get first directory
+          local parts = vim.split(relative_path, '/')
+          local first_depth = parts[1]
+
+          builtin.git_status {
+            cwd = first_depth,
+          }
+        end)
       end,
     },
   },
